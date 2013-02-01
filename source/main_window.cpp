5622c2a26a21545b7b0561eb326dda24b376fc0e
Peter Cawley <corsix@corsix.org>
2013-02-01 00:09:47 +0000

    Zoom camera according to model size.

--- source/main_window.cpp
+++ source/main_window.cpp
@@ -61,19 +61,31 @@ void MainWindow::setModel(Essence::FileSource* mod_fs, const std::string& path)
   try
   {
     m_model.reset(new Essence::Graphics::Model(mod_fs, m_essence->getShaders(), mod_fs->readFile(path), m_essence->getDevice()));
+
+    m_model_size = 1.f;
+    m_model->getMeshes().for_each([&](Essence::Graphics::Mesh* mesh)
+    {
+      auto& bvol = mesh->getBoundingVolume();
+      const float x = bvol.center.x + bvol.size.x;
+      const float y = bvol.center.y + bvol.size.y;
+      const float z = bvol.center.z + bvol.size.z;
+      const float size = sqrt(x*x + y*y + z*z);
+      if(m_model_size < size)
+        m_model_size = size;
+    });
   }
   catch(const std::exception& e)
   {
     ::wxMessageBox(wxString("Error whilst loading " + path + ":\n" + e.what()), L"Exception", wxICON_EXCLAMATION | wxOK, this);
     return;
   }
-  m_essence->Refresh(false);
+  updateCamera();
 }
 
 void MainWindow::updateCamera()
 {
   float a = (static_cast<float>(m_camera_angle) / 16.f) * M_PI;
-  float dist = 8.f / (1.f + abs(m_camera_height - 2.f) * 0.1f);
+  float dist = m_model_size / (1.f + abs(m_camera_height - 2.f) * 0.1f);
   Essence::Graphics::vector3_t eye = {cos(a) * dist, m_camera_height, sin(a) * dist};
   Essence::Graphics::vector3_t at = {0.f, (m_camera_height - 2.f) * 0.01f, 0.f};
   m_essence->lookAtFrom(at, eye);
--- source/main_window.h
+++ source/main_window.h
@@ -19,5 +19,6 @@ private:
   Essence::Graphics::Panel* m_essence;
   int m_camera_angle;
   float m_camera_height;
+  float m_model_size;
   std::unique_ptr<Essence::Graphics::Model> m_model;
 };
